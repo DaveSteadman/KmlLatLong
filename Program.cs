080b2aec9a305de9ae76dc6f08f9f8301acdfca6
Dave Steadman <davesteadman@me.com>
2024-01-04 16:47:46 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -67,6 +67,7 @@ class KmlGenerator
 
         File.WriteAllText(filePath, kmlContent.ToString());
     }
+
     public void GenerateKmlFile_0p2Deg()
     {
         string filePath = "KmlLatLong_0p2Deg.kml";
@@ -88,6 +89,98 @@ class KmlGenerator
         File.WriteAllText(filePath, kmlContent.ToString());
     }
 
+    // --------------------------------------------------------------------------------------------
+
+    public void GenerateKmlFile_45DegLabels()
+    {
+        string filePath = "KmlLatLong_45Deg_Labels.kml";
+        var kmlContent = new StringBuilder();
+
+        WriteKmlHeader(kmlContent);
+
+        // Generate Latitude Lines
+        string styleUrl = "LabelStyleLevel0";
+
+        for (int lat = -80; lat <=80; lat+=40)
+        {
+            float adjLon = -180f + 22.5f;
+            AddLabel(kmlContent, lat, adjLon, styleUrl, $"Lat {lat}");
+        }
+
+        for (int lon = -180; lon<=180; lon+=45)
+        {
+            float adjLat = -60f;
+            AddLabel(kmlContent, adjLat, lon, styleUrl, $"Lon {lon:F0}");
+        }
+
+        WriteKmlFooter(kmlContent);
+        File.WriteAllText(filePath, kmlContent.ToString());
+    }
+
+    public void GenerateKmlFile_5DegLabels()
+    {
+        string filePath = "KmlLatLong_5Deg_Labels.kml";
+        var kmlContent = new StringBuilder();
+
+        WriteKmlHeader(kmlContent);
+
+        // Generate Latitude Lines
+        string styleUrl = "LabelStyleLevel1";
+
+        for (int lat = -80; lat <=80; lat+=5)
+        {
+            for (int lon = -180; lon<=180; lon+=45)
+            {
+                float adjLon = lon + 2.5f;
+                AddLabel(kmlContent, lat, adjLon, styleUrl, $"Lat {lat}");
+            }
+        }
+
+        for (int lon = -180; lon<=180; lon+=5)
+        {
+            for (int lat = -80; lat <=79; lat+=40)
+            {
+                float adjLat = lat + 2.5f;
+                AddLabel(kmlContent, adjLat, lon, styleUrl, $"Lon {lon:F0}");
+            }
+
+        }
+
+        WriteKmlFooter(kmlContent);
+        File.WriteAllText(filePath, kmlContent.ToString());
+    }
+
+    public void GenerateKmlFile_1DegLabels()
+    {
+        string filePath = "KmlLatLong_1Deg_Labels.kml";
+        var kmlContent = new StringBuilder();
+
+        WriteKmlHeader(kmlContent);
+
+        // Generate Latitude Lines
+        string styleUrl = "LabelStyleLevel2";
+
+        for (int lat = -80; lat <=80; lat+=1)
+        {
+            for (int lon = -180; lon<=180; lon+=5)
+            {
+                float adjLon = lon + 0.5f;
+                AddLabel(kmlContent, lat, adjLon, styleUrl, $"Lat {lat}");
+            }
+        }
+
+        for (int lon = -180; lon<=180; lon+=1)
+        {
+            for (int lat = -80; lat <=79; lat+=5)
+            {
+                float adjLat = lat + 0.5f;
+                AddLabel(kmlContent, adjLat, lon, styleUrl, $"Lon {lon}");
+            }
+        }
+
+        WriteKmlFooter(kmlContent);
+        File.WriteAllText(filePath, kmlContent.ToString());
+    }
 
     // --------------------------------------------------------------------------------------------
 
@@ -124,15 +217,12 @@ class KmlGenerator
         File.WriteAllText(filePath, kmlContent.ToString());
     }
 
-
-
     private void WriteKmlHeader(StringBuilder kmlContent)
     {
         kmlContent.AppendLine("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
         kmlContent.AppendLine("<kml xmlns=\"http://www.opengis.net/kml/2.2\">");
         kmlContent.AppendLine("<Document>");
         kmlContent.AppendLine("<name>Latitude and Longitude Grid</name>");
-
         AddStyles(kmlContent);
     }
 
@@ -145,56 +235,27 @@ class KmlGenerator
     private void AddStyles(StringBuilder kmlContent)
     {
         // Level0 Style
-        kmlContent.AppendLine("  <Style id=\"Level0\">");
-        kmlContent.AppendLine("    <LineStyle>");
-        kmlContent.AppendLine("      <color>ff0000ff</color>"); // Red color for Level0
-        kmlContent.AppendLine("      <width>3</width>"); // Thicker line for Level0
-        kmlContent.AppendLine("    </LineStyle>");
-        kmlContent.AppendLine("  </Style>");
+        kmlContent.AppendLine("<Style id=\"Level0\"><LineStyle><color>ff0000ff</color><width>3</width></LineStyle></Style>");
+        kmlContent.AppendLine("<Style id=\"Level1\"><LineStyle><color>ff00ff00</color><width>2</width></LineStyle></Style>");
+        kmlContent.AppendLine("<Style id=\"Level2\"><LineStyle><color>ffff0000</color><width>1</width></LineStyle></Style>");
+        kmlContent.AppendLine("<Style id=\"Level3\"><LineStyle><color>66666666</color><width>1</width></LineStyle></Style>");
 
-        // Level1 Style
-        kmlContent.AppendLine("  <Style id=\"Level1\">");
-        kmlContent.AppendLine("    <LineStyle>");
-        kmlContent.AppendLine("      <color>ff00ff00</color>"); // Green color for Level1
-        kmlContent.AppendLine("      <width>2</width>"); // Medium line for Level1
-        kmlContent.AppendLine("    </LineStyle>");
-        kmlContent.AppendLine("  </Style>");
-
-        // Level2 Style
-        kmlContent.AppendLine("  <Style id=\"Level2\">");
-        kmlContent.AppendLine("    <LineStyle>");
-        kmlContent.AppendLine("      <color>ffff0000</color>"); // Blue color for Level2
-        kmlContent.AppendLine("      <width>1</width>"); // Thinner line for Level2
-        kmlContent.AppendLine("    </LineStyle>");
-        kmlContent.AppendLine("  </Style>");
-
-        // Level2 Style
-        kmlContent.AppendLine("  <Style id=\"Level3\">");
-        kmlContent.AppendLine("    <LineStyle>");
-        kmlContent.AppendLine("      <color>777777ff</color>"); // Blue color for Level2
-        kmlContent.AppendLine("      <width>1</width>"); // Thinner line for Level2
-        kmlContent.AppendLine("    </LineStyle>");
-        kmlContent.AppendLine("  </Style>");
-
-        // Text Label Style
-        kmlContent.AppendLine("  <Style id=\"textLabelStyle\">");
-        kmlContent.AppendLine("    <LabelStyle>");
-        kmlContent.AppendLine("      <color>ff0000ff</color>"); // Example: blue color (ARGB format)
-        kmlContent.AppendLine("      <scale>1.5</scale>"); // Example: 1.5 times the normal size
-        kmlContent.AppendLine("    </LabelStyle>");
-        kmlContent.AppendLine("  </Style>");
+        // Level Label Style
+        kmlContent.AppendLine("<Style id=\"LabelStyleLevel0\"><LabelStyle><color>ff0000ff</color><scale>1.0</scale></LabelStyle></Style>");
+        kmlContent.AppendLine("<Style id=\"LabelStyleLevel1\"><LabelStyle><color>ff00ff00</color><scale>1.0</scale></LabelStyle></Style>");
+        kmlContent.AppendLine("<Style id=\"LabelStyleLevel2\"><LabelStyle><color>ffffdddd</color><scale>1.0</scale></LabelStyle></Style>");
     }
 
     private void AddLatLine(StringBuilder kmlContent, float latitude, string styleUrl)
     {
-        kmlContent.AppendLine("  <Placemark>");
+        kmlContent.AppendLine( "  <Placemark>");
         kmlContent.AppendLine($"    <styleUrl>#{styleUrl}</styleUrl>");
-        kmlContent.AppendLine("    <LineString>");
-        kmlContent.AppendLine("      <coordinates>");
+        kmlContent.AppendLine( "    <LineString>");
+        kmlContent.AppendLine( "      <coordinates>");
         kmlContent.AppendLine($"        -180,{latitude:F3},0 180,{latitude:F3},0");
-        kmlContent.AppendLine("      </coordinates>");
-        kmlContent.AppendLine("    </LineString>");
-        kmlContent.AppendLine("  </Placemark>");
+        kmlContent.AppendLine( "      </coordinates>");
+        kmlContent.AppendLine( "    </LineString>");
+        kmlContent.AppendLine( "  </Placemark>");
     }
 
     private void AddLonLine(StringBuilder kmlContent, float longitude, string styleUrl)
@@ -229,8 +290,14 @@ class Program
     {
         var generator = new KmlGenerator();
         generator.GenerateKmlFile_45Deg();
+        generator.GenerateKmlFile_45DegLabels();
+
         generator.GenerateKmlFile_5Deg();
+        generator.GenerateKmlFile_5DegLabels();
+
         generator.GenerateKmlFile_1Deg();
+        generator.GenerateKmlFile_1DegLabels();
+
         generator.GenerateKmlFile_0p2Deg();
 
         generator.GenerateKmlFile_Lvl0TileCodes();
